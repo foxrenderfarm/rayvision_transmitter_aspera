e3d555aef669b7d1506bf71e9066d09fa27d7e0c
dan <1113965383@qq.com>
2020-05-28 15:57:18 +0800

    feat: aspera transmit

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""Describe our module distribution to Distutils."""
+
+# Import third-party modules
+from setuptools import find_packages
+from setuptools import setup
+
+
+def parse_requirements(filename):
+    with open(filename, "r") as f:
+        for line in f:
+            yield line.strip()
+
+
+setup(
+    name="rayvision_transmitter_aspera",
+    author="Shenzhen Rayvision Technology Co., Ltd",
+    author_email="developer@rayvision.com",
+    version="1.0",
+    url="",
+    package_dir={"": "."},
+    packages=find_packages("."),
+    description=("aspera transfer tool, including window and centos versions"),
+    entry_points={},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    # use_scm_version=True,
+    # setup_requires=["setuptools_scm"],
+)
